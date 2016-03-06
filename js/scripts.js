642ad9640aa9ddeb60cae691702874fb10944da8
shtativ <xiamkaix@gmail.com>
2016-03-06 03:12:11 +0300

    add a bit jquery /menu, slow transition/

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
+//hide menu
 $('a#header-menu').click(function () {
     $('#menu').fadeIn('slow');
 });
-
 //show menu
 $('#menu a').click(function () {
     $('#menu').fadeOut('slow');
 });
+ 
+    $("#pointer-down").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
