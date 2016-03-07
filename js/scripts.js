19cc342c5a638ab98e5c5ee9bc08a2635b4e7082
shtativ <xiamkaix@gmail.com>
2016-03-07 03:51:49 +0300

    fix menu and btn

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,14 @@ $('a#header-menu').click(function () {
 $('#menu a').click(function () {
     $('#menu').fadeOut('slow');
 });
- 
-    $("#pointer-down").on("click","a", function (event) {
+ //slow transition 
+//    $("#pointer-down").on("click","a", function (event) { 
+    $('#pointer-down').click(function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    }); $('#btns').on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
