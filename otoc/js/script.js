9548ee86ac6c131572617141491453528c9f1f99
shtativ <xiamkaix@gmail.com>
2016-04-01 01:50:24 +0300

    add animation in works-block

--- otoc/js/script.js
+++ otoc/js/script.js
@@ -10,4 +10,25 @@ $(function(){
   animateOut: 'slideOutUp',
   animateIn: 'slideInUp'
   });
+	//scroll
+//   $('a[href*=#]').bind("click", function(e){
+//      var anchor = $(this);
+//      $('html, body').stop().animate({
+//         scrollTop: $(anchor.attr('href')).offset().top
+//      }, 1000);
+//      e.preventDefault();
+//   });
+//   return false;
+//});
+	$('a[href^="#"]').click(function () { 
+ elementClick = $(this).attr("href");
+ destination = $(elementClick).offset().top;
+ if($.browser.safari){
+ $('body').animate( { scrollTop: destination }, 1000 );
+ } else {
+  $('html').animate( { scrollTop: destination }, 1000 );
+ }
+ return false;
 });
+});
+
