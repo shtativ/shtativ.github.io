580ce44f3bfba20a48287fab8eb6ef2597f6a420
shtativ <xiamkaix@gmail.com>
2016-04-02 01:08:05 +0300

    include slow scroll

--- otoc/js/script.js
+++ otoc/js/script.js
@@ -1,34 +1,28 @@
-$(function(){
-  $(".owl-carousel").owlCarousel({
-	items:1,
-	margin:10,
-	loop:true,
-	nav:true,
-	autoplay:true,
-	mouseDrag:false,
-  autoplayHoverPause: true,
-  animateOut: 'slideOutUp',
-  animateIn: 'slideInUp'
-  });
-	//scroll
-//   $('a[href*=#]').bind("click", function(e){
-//      var anchor = $(this);
-//      $('html, body').stop().animate({
-//         scrollTop: $(anchor.attr('href')).offset().top
-//      }, 1000);
-//      e.preventDefault();
-//   });
-//   return false;
-//});
-	$('a[href^="#"]').click(function () { 
- elementClick = $(this).attr("href");
- destination = $(elementClick).offset().top;
- if($.browser.safari){
- $('body').animate( { scrollTop: destination }, 1000 );
- } else {
-  $('html').animate( { scrollTop: destination }, 1000 );
- }
- return false;
-});
-});
+$(function () {
+	//carousel
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		margin: 10,
+		loop: true,
+		nav: true,
+		autoplay: true,
+		mouseDrag: false,
+		autoplayHoverPause: true,
+		animateOut: 'slideOutUp',
+		animateIn: 'slideInUp'
+	});
 
+	//slow scroll
+	$('a[href*="#"]').click(function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top }, 555);
+		if ($.browser.safari) {
+			$('body').animate({scrollTop: top }, 555);
+		} else {
+			$('body, html').animate({scrollTop: top }, 555);
+		}
+		return false;
+	});
+});
