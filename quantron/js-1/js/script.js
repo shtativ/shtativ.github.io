78d18b0dcc36664b449119b36f342892c267a290
shtativ <xiamkaix@gmail.com>
2016-06-06 00:53:56 +0300

    add materialize folder

--- quantron/js-1/js/script.js
+++
@@ -1,8 +0,0 @@
-// animation green buttons
-
-$('#red_button').click(
-       function() {
-           $('#green_btns').toggleClass('animated fadeInDownBig');
-           $("#green_btns").css('display', 'flex');
-      }
-   );
