b76cbabb91b8f8934135543c5b2e01a800e3c2b7
shtativ <xiamkaix@gmail.com>
2016-06-27 04:05:49 +0300

    add responsive index-page / add hamburger menu / jquery v.2

--- tender-partner/js/scripts.js
+++ tender-partner/js/scripts.js
@@ -1,7 +1,17 @@
-
 //hamburger menu
-  var $hamburger = $(".hamburger");
-  	$hamburger.on("click", function(e) {
-	  	$hamburger.toggleClass("is-active");
-	  	$('#nav_items').slideToggle('slow');
-  });
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    $('#nav_items').slideToggle('slow');
+});
+
+//hover fade menu
+$('.myMenu ul li').hover(function() {
+	$(this).children('ul li').stop(true, false, true).fadeToggle(300);
+});
+//
+// $('.myMenu ul li.has-children > a').click(function() {
+// 	$(this).parent().siblings().find('ul').slideUp(300);
+// 	$(this).next('ul').stop(true, false, true).slideToggle(300);
+// 	return false;
+// });
