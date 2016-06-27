2b9587d01e245a3881c42c60fe9fb412213cb026
shtativ <xiamkaix@gmail.com>
2016-06-28 02:51:05 +0300

    added a hamburger-menu of all pages / added responsive to all pages

--- tender-partner/js/scripts.js
+++ tender-partner/js/scripts.js
@@ -4,14 +4,3 @@ $hamburger.on("click", function(e) {
     $hamburger.toggleClass("is-active");
     $('#nav_items').slideToggle('slow');
 });
-
-//hover fade menu
-$('.myMenu ul>li').hover(function() {
-	$(this)('ul>li').fadeToggle(300);
-});
-//
-// $('.myMenu ul li.has-children > a').click(function() {
-// 	$(this).parent().siblings().find('ul').slideUp(300);
-// 	$(this).next('ul').stop(true, false, true).slideToggle(300);
-// 	return false;
-// });
