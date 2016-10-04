71925f2f67615d95c79b0571ed5985a6050d0039
shtativ <xiamkaix@gmail.com>
2016-10-04 21:58:22 +0300

    browserSync

--- examples/7/gulpfile.js
+++ examples/7/gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 postcss([require('postcss-flexbugs-fixes')]);
+var browserSync = require('browser-sync');
+
 gulp.task('css', function () {
     var processors = [
         autoprefixer({browsers: ['last 2 version']['> 2%']}),
@@ -11,6 +13,17 @@ gulp.task('css', function () {
         .pipe(postcss(processors))
         .pipe(gulp.dest('css'));
 });
-
+gulp.task('browser-sync', function () {
+	var files = [
+		'*.html',
+		'css/**/*.css',
+		'img/**/*.png'
+		];
+	browserSync.init(files, {
+		server: {
+			baseDir: '.'
+			}
+		});
+	});
 gulp.task('default', ['css']);
 
