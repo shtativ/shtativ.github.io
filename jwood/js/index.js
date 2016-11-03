2860e3fa6b7ec008e7a4785e858f31bc0d427bbb
vkrms <april504@yandex.ru>
2016-11-03 11:40:57 +1000

    add jquery em scaling plugin

--- jwood/js/index.js
+++ jwood/js/index.js
@@ -73,6 +73,24 @@ $(document).ready(function() {
         afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {},
         onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {}
     });
+
+
+    //scaler
+    $.fn.scale = function(){
+      var ww = $(window).width();
+      var wh = $(window).height();
+      var c = 1.5; //коэффициент, чтобы все не было слишком мелким
+      var fs = (ww * wh)/(1920 * 955) * 100 * c + '%';
+      console.log(fs);
+      $(this).css('font-size', fs);
+    }
+
+    $('section').scale();
+
+    $(window).on('resize',function(){
+      $('section').scale();
+    });
+
 });
 
 //old pullpage plugin
