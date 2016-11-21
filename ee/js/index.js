6754e8eeb0826f4c76055dfcffd91f31dcccb3d3
shtativ <xiamkaix@gmail.com>
2016-11-22 00:49:48 +0300

    broke fullpage.js plugin

--- ee/js/index.js
+++ ee/js/index.js
@@ -14,8 +14,81 @@ lightbox.option({
      'positionFromTop': 30,
      'wrapAround': true
    })
- // });
-//
+ //
+ $(document).ready(function() {
+     $('#fullpage').fullpage();
+ });
+//FullPage scroll
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['firstPage', 'secondPage', '3thPage', '4thPage'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: false,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : ['#bebebe', '#7e7d7d'],
+        paddingTop: '5px',
+        paddingBottom: '5px',
+        fixedElements: null,
+        // fixedElements: '#header, .footer',
+        responsiveWidth: 900,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+});
+
 // $(document).ready(function() {
 //     $('#fullpage').fullpage({
 //         //Navigation
@@ -24,7 +97,7 @@ lightbox.option({
 //         anchors: ['firstPage', 'secondPage', '3thPage', '4thPage'],
 //         navigation: true,
 //         navigationPosition: 'right',
-//         navigationTooltips: 0,
+//         navigationTooltips: ['firstSlide', 'secondSlide'],
 //         showActiveTooltip: false,
 //         slidesNavigation: true,
 //         slidesNavPosition: 'bottom',
@@ -66,7 +139,7 @@ lightbox.option({
 //         paddingTop: '10px', //'1em',
 //         paddingBottom: '0', //'10px',
 //         fixedElements: null,
-//         responsiveWidth: 0,
+//         responsiveWidth: 900,
 //         responsiveHeight: 0,
 //         responsiveSlides: true,
 //
@@ -83,35 +156,4 @@ lightbox.option({
 //         afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {},
 //         onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {}
 //     });
-//});
-
-
-    //scaler
-    // $.fn.scale = function() {
-    //     var ww = $(window).width();
-    //     var wh = $(window).height();
-    //     var c = 1.5; //коэффициент, чтобы все не было слишком мелким
-    //     var fs = (ww * wh) / (1920 * 955) * 100 * c;
-    //     console.log(fs);
-    //     $(this).css('font-size', fs);
-    //     if (fs >= 100) return;
-    //     $(this).css('font-size', fs + '%');
-    // }
-
-    // $('section').scale();
-    //
-    // $(window).on('resize', function() {
-    //     $('section').scale();
-    // });
-
-
-
-//custom plugin
-// $.fn.resize = function(){
-//   size = (($(window).height()*$(window).width())/(1920*1080)) * 100 + "%"
-//   console.log(size);
-//   $(this).css('font-size',size);
-// }
-// $(window).on('resize',function(){
-//   $('.section').resize();
 // });
