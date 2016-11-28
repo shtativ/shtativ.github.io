e1d9f984b43c14795ced1d262596c44478e0b0bc
shtativ <xiamkaix@gmail.com>
2016-11-28 20:52:20 +0300

    minify and compress code

--- ee/gulpfile.js
+++ ee/gulpfile.js
@@ -4,21 +4,38 @@ var autoprefixer = require('autoprefixer');
 postcss([require('postcss-flexbugs-fixes')]);
 var browserSync = require('browser-sync');
 var imagemin = require('gulp-imagemin');
-postcss([require('postcss-flexbugs-fixes')]);
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+var htmlmin = require('gulp-htmlmin');
 
-
+//minify-css
 gulp.task('css', function () {
     var processors = [
         autoprefixer({browsers: ['last 2 version']['> 2%']}),
     ];
     return gulp.src('css/**/*.css')
         .pipe(postcss(processors))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(csso())
         .pipe(gulp.dest('dist/css/'));
 });
+//minify-html
+gulp.task('html', function() {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'));
+});
+//minify-js
+gulp.task('js', function () {
+  return gulp.src('js/**/*.js')
+       .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
 
-//compress images task
-gulp.task('compress', function () {
-    gulp.src('images/**/*')
+});
+//compress images
+gulp.task('img', function () {
+    gulp.src('images/**/*.*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images/'))
 });
