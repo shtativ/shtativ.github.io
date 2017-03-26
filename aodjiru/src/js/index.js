cf59ab576cd879987f9b2a2ae3ca496526de4f02
shtativ <xiamkaix@gmail.com>
2017-03-26 07:03:22 +0300

    page v 0.2

--- aodjiru/src/js/index.js
+++ aodjiru/src/js/index.js
@@ -10,3 +10,27 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   )
   document.head.appendChild(msViewportStyle)
 }
+  $(document).ready(function () {
+    //initialize swiper when document ready  
+    var mySwiper = new Swiper ('.swiper-container', {
+      // spaceBetween: 100,
+      grabCursor: true,
+      slidesPerView: 3,
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      loop: true
+      //,
+      //  breakpoints: {
+      //           420: {
+      //               slidesPerGroup: 1,
+      //               slidesPerView: 1,
+      //               spaceBetween: 10
+      //           },
+      //           640: {
+      //               slidesPerGroup: 2,
+      //               slidesPerView: 2,
+      //               spaceBetween: 20
+      //           }
+      //       }
+    })        
+  });
